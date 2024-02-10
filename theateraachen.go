37dfa3fa984f18cb84a1bb50994a17a3825b6207
Florian Hülsmann <fh@cbix.de>
2024-02-10 15:13:40 +0100

    parse reservix impressions

--- theateraachen.go
+++ theateraachen.go
@@ -1,48 +1,112 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 
-	"golang.org/x/net/html"
+	ics "github.com/arran4/golang-ical"
 )
 
 func init() {
-	RegisterAdapter("theateraachen", theateraachenUrl, "Theatre in Aachen", theateraachenAdapter)
+	RegisterAdapter("theateraachen", theateraachenUrl, "Theatre in Aachen (beta)", theateraachenAdapter)
+}
+
+type theateraachenData struct {
+	Ecommerce theateraachenEcommerce `json:"ecommerce"`
+}
+
+type theateraachenEcommerce struct {
+	Impressions []theateraachenImpression `json:"impressions"`
+}
+
+type theateraachenImpression struct {
+	Id       string               `json:"id"`
+	Name     string               `json:"name"`
+	Variant  theateraachenVariant `json:"variant"`
+	Position int                  `json:"position"`
+}
+
+type theateraachenVariant struct {
+	DateTime  string `json:"dateTime"`
+	VenueName string `json:"venueName"`
+	GenreName string `json:"genreName"`
+}
+
+func (i theateraachenImpression) StartTime() time.Time {
+	t, _ := time.ParseInLocation(theateraachenLocalFormat, i.Variant.DateTime, theateraachenTZ)
+	return t
+}
+
+func (i theateraachenImpression) EndTime() time.Time {
+	return i.StartTime().Add(2 * time.Hour)
 }
 
 const (
-	theateraachenUrl = "https://www.theateraachen.de/"
-	//theateraachenSrc = "https://www.theateraachen.de/de/spielplan/kalender.html?ajax=1&offset=%d"
-	theateraachenSrc = "https://theateraachen.reservix.de/events/%d"
+	theateraachenUrl         = "https://www.theateraachen.de/"
+	theateraachenSrc         = "https://theateraachen.reservix.de/events/%d"
+	theateraachenJsonPrefix  = "var impressionData = "
+	theateraachenLocalFormat = "2006-01-02 15:04:05"
 )
 
+var theateraachenTZ, _ = time.LoadLocation("Europe/Berlin")
+
 func theateraachenAdapter(w io.Writer) error {
-	n := 0
+	cal := ics.NewCalendarFor("github.com/cbix/ics")
+	cal.SetName("Theater Aachen")
+	cal.SetUrl(theateraachenUrl)
+	cal.SetTimezoneId("Europe/Berlin")
+	now := time.Now()
+	pos := 0
+page:
 	for i := 1; i < 10; i++ {
-		res, err := http.Get(fmt.Sprintf(theateraachenSrc, i))
+		req, err := http.NewRequest("GET", fmt.Sprintf(theateraachenSrc, i), nil)
 		if err != nil {
 			return err
 		}
-		nodes, err := html.Parse(res.Body)
-		res.Body.Close()
+		req.Header.Set("User-Agent", "Mozilla/5.0 Gecko/20100101 Firefox/122.0")
+		res, err := http.DefaultClient.Do(req)
 		if err != nil {
 			return err
 		}
-		startNode := nodes.FirstChild.FirstChild.NextSibling.FirstChild
-		if startNode.FirstChild == nil {
+		scanner := bufio.NewScanner(res.Body)
+		var jsonData []byte
+		for scanner.Scan() {
+			l := strings.Trim(scanner.Text(), " ;")
+			if strings.HasPrefix(l, theateraachenJsonPrefix) {
+				jsonData = []byte(strings.TrimPrefix(l, theateraachenJsonPrefix))
+				break
+			}
+		}
+		if len(jsonData) == 0 {
+			continue
+		}
+		var data theateraachenData
+		err = json.Unmarshal(jsonData, &data)
+		if err != nil {
+			return err
+		}
+		if len(data.Ecommerce.Impressions) == 0 {
 			break
 		}
-		//fmt.Fprintf(w, "startNode: %+v\n", startNode)
-		for node := startNode; node != nil; node = node.NextSibling {
-			if node.FirstChild == nil || node.FirstChild.NextSibling == nil {
-				continue
+		for _, ev := range data.Ecommerce.Impressions {
+			p := ev.Position
+			if p <= pos {
+				break page
 			}
-			n++
-			fmt.Fprintf(w, "child: %+v\n", node.FirstChild.NextSibling)
+			pos = p
+			event := cal.AddEvent(fmt.Sprintf("%s@theateraachen.reservix.de", ev.Id))
+			event.SetDtStampTime(now)
+			event.SetSummary(ev.Name)
+			event.SetDescription(ev.Variant.GenreName)
+			event.SetLocation(ev.Variant.VenueName)
+			event.SetStartAt(ev.StartTime())
+			event.SetEndAt(ev.EndTime())
 		}
 	}
-	fmt.Fprintf(w, "nodes: %d\n", n)
-	return nil
+	return cal.SerializeTo(w)
 }
